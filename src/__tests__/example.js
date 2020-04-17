eb82e0aac2d184329a97f4ad09fc832b105d8439
bartosz.ciesielczyk <bartosz.ciesielczyk@meelogic.com>
2020-04-17 13:54:51 +0200

    connect codecov

---
+++ src/__tests__/example.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import CalculatorWindow from "../components/CalculatorWindow";
+
+test("renders", () => {
+  const { container } = render(<CalculatorWindow value="0" />);
+  expect(container).toMatchInlineSnapshot(`
+    .emotion-0 {
+      background: #a6a1a0;
+      text-align: right;
+      height: 25vh;
+      font-size: 15vmin;
+    }
+
+    <div>
+      <div
+        class="emotion-0"
+      >
+        0
+      </div>
+    </div>
+  `);
+});
--- src/components/CalculatorWindow.jsx
+++ src/components/CalculatorWindow.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import styled from "@emotion/styled";
 
 const Container = styled.div`
-  background: #a6a1a1;
+  background: #a6a1a0;
   text-align: right;
   height: 25vh;
   font-size: 15vmin;
 `;
 
-const CalculatorWindow = props => {
+const CalculatorWindow = (props) => {
   return <Container>{props.value ? props.value : props.total}</Container>;
 };
 
